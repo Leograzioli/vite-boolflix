94b4780c749e54cbff22f686e947e503cc4aba07
Leograzioli <leograzioli@gmail.com>
2022-11-14 16:59:36 +0100

    aggiunto efect al hover

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -11,19 +11,51 @@ export default {
 
 <template>
     
-    <div>
-        <p>Title: {{ name }}</p>
-        <p>Original Title: {{ originalName }}</p>
-        <p v-if="movieItem.original_language === 'en'"><img src="../assets/tn_uk-flag.jpg" alt=""></p>
-        <p v-else-if="movieItem.original_language === 'it'"><img src="../assets/tn_it-flag.jpg" alt=""></p>
-        <p v-else-if="movieItem.original_language === 'de'"><img src="../assets/tn_gm-flag.jpg" alt=""></p>
-        <p v-else>Original Language: {{ movieItem.original_language }}</p>
-        <p v-if="movieItem.vote_average > 0">rate: <i v-for="index in 5" v-show=" Math.ceil(movieItem.vote_average / 2) >= index" class="fa-solid fa-star"></i></p>
-        <p v-else>rate: <i class="fa-regular fa-star"></i></p>
-        <img :src="'https://image.tmdb.org/t/p/w185' + movieItem.poster_path" alt="">
+    <div class="ms_card">
+        <div class="ms_description p-3">
+            <p>Title: {{ name }}</p>
+            <p>Original Title: {{ originalName }}</p>
+            <p v-if="movieItem.original_language === 'en' || movieItem.original_language === 'sh'"><img src="../assets/tn_uk-flag.jpg" alt=""></p>
+            <p v-else-if="movieItem.original_language === 'it'"><img src="../assets/tn_it-flag.jpg" alt=""></p>
+            <p v-else-if="movieItem.original_language === 'de'"><img src="../assets/tn_gm-flag.jpg" alt=""></p>
+            <p v-else>Original Language: {{ movieItem.original_language }}</p>
+            <p v-if="movieItem.vote_average > 0">rate: <i v-for="index in 5" v-show=" Math.ceil(movieItem.vote_average / 2) >= index" class="fa-solid fa-star"></i></p>
+            <p v-else>rate: <i class="fa-regular fa-star"></i></p>
+            <p>{{movieItem.overview}}</p>
+        </div>
+
+        <div class="ms_image">
+            <img :src="'https://image.tmdb.org/t/p/w342' + movieItem.poster_path" alt="">
+        </div>
     </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.ms_card {
+    position: relative;
+    height: 100%;
+    background-color: rgb(27, 27, 27);
+    overflow: auto;
+
+    .ms_description {
+
+        img{
+            width: 30px;
+        }
+    }
+
+}
+.ms_card:hover .ms_description {
+    display: block;
+}
+.ms_card:hover .ms_image {
+    display: none;
+}
+.ms_description {
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: none;
+}
 
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,19 +18,25 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-
-        <!-- movie section  -->
+    <div class="container-fluid bg-dark">
         <h2>movies</h2>
-        <div class="col" v-for="(item, index) in store.moviesArray" :key="index">
-            <AppCard :name="item.title" :original-name="item.original_title" :movieItem="item" />       
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 text-white g-3">
+            <!-- movie section  -->
+            <div class="col" v-for="(item, index) in store.moviesArray" :key="index">
+                <AppCard :name="item.title" :original-name="item.original_title" :movieItem="item" />       
+            </div>
         </div>
 
-        <!-- serie section  -->
         <h2>series</h2>
-        <div class="col" v-for="(item, index) in store.seriesArray" :key="index">
-            <AppCard :name="item.name" :original-name="item.original_name" :movieItem="item" />       
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 text-white g-3">
+            <!-- serie section  -->
+            <div class="col" v-for="(item, index) in store.seriesArray" :key="index">
+                <AppCard :name="item.name" :original-name="item.original_name" :movieItem="item" />       
+            </div>
+
         </div>
+
+
     </div>
 </template>
 
