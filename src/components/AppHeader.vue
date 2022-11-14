560dae54f613ad82e7b2dc5938fe0cd3a2ea346a
Leograzioli <leograzioli@gmail.com>
2022-11-14 16:18:53 +0100

    style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,10 +11,20 @@ export default {
 </script>
 
 <template>
-    <div>
-        <input type="text" @keyup.enter="$emit('clickedButton')" v-model="store.inputSearch">
-        <button @click="$emit('clickedButton')" >vai!</button>
-    </div>
+    <header class="d-flex justify-content-between py-3 px-4 bg-black">
+        <div class="logo">
+            <img src="https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png" alt="">
+        </div>
+        <div>
+            <label class="d-none" for="search-bar">Search movie</label>
+            <input id="search-bar" type="text" @keyup.enter="$emit('clickedButton')" v-model="store.inputSearch">
+            <button @click="$emit('clickedButton')" >vai!</button>
+        </div>
+    </header>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.logo{
+    img{ width: 140px;}
+}
+</style>
