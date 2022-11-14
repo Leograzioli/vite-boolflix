ff9286a9358a5c2abe055306863f9f2485a40890
Leograzioli <leograzioli@gmail.com>
2022-11-14 15:28:50 +0100

    reso componente unico per serie e movie

---
+++ src/components/AppCard.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'MovieCard',
+    props: {
+        movieItem: Object,
+        name: String,
+        originalName: String
+    }
+
+}
+</script>
+
+<template>
+    
+    <div>
+        <p>Title: {{ name }}</p>
+        <p>Original Title: {{ originalName }}</p>
+        <p v-if="movieItem.original_language === 'en'"><img src="../assets/tn_uk-flag.jpg" alt=""></p>
+        <p v-else-if="movieItem.original_language === 'it'"><img src="../assets/tn_it-flag.jpg" alt=""></p>
+        <p v-else-if="movieItem.original_language === 'de'"><img src="../assets/tn_gm-flag.jpg" alt=""></p>
+        <p v-else>Original Language: {{ movieItem.original_language }}</p>
+        <p>rate:{{ parseInt(movieItem.vote_average / 2) }}</p>
+        <img :src="'https://image.tmdb.org/t/p/w185' + movieItem.poster_path" alt="">
+    </div>
+</template>
+
+<style>
+
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,5 @@
 <script>
+import AppCard from './AppCard.vue';
 import { store } from '../store'
 export default {
     name: 'AppMain',
@@ -6,34 +7,29 @@ export default {
         return {
             store,
         }
+    }, 
+    methods: {
+    
+    },
+    components: {
+        AppCard,
     }
 }
 </script>
 
 <template>
     <div class="container">
-        <h2>film</h2>
-        <div v-for="(item, index) in store.moviesArray" :key="index">
-            <p>Title: {{item.title}}</p>
-            <p>Original Title: {{item.original_title}}</p>
-            <p v-if="item.original_language === 'en'"><img src="../assets/tn_uk-flag.jpg" alt=""> / <img src="../assets/tn_us-flag.jpg" alt=""></p>
-            <p v-else-if="item.original_language === 'it'"><img src="../assets/tn_it-flag.jpg" alt=""></p>
-            <p v-else-if="item.original_language === 'de'"><img src="../assets/tn_gm-flag.jpg" alt=""></p>
-            <p v-else>Original Language: {{item.original_language}}</p>
-            <p>rate:{{parseInt(item.vote_average / 2)}}</p>
-            <img :src="'https://image.tmdb.org/t/p/w185' + item.poster_path" alt="">
+
+        <!-- movie section  -->
+        <h2>movies</h2>
+        <div class="col" v-for="(item, index) in store.moviesArray" :key="index">
+            <AppCard :name="item.title" :original-name="item.original_title" :movieItem="item" />       
         </div>
 
+        <!-- serie section  -->
         <h2>series</h2>
-        <div v-for="(item, index) in store.seriesArray" :key="index">
-            <p>Title: {{item.name}}</p>
-            <p>Original title: {{item.original_name}}</p>
-            <p v-if="item.original_language === 'en'"><img src="../assets/tn_uk-flag.jpg" alt=""> / <img src="../assets/tn_us-flag.jpg" alt=""></p>
-            <p v-else-if="item.original_language === 'it'"><img src="../assets/tn_it-flag.jpg" alt=""></p>
-            <p v-else-if="item.original_language === 'de'"><img src="../assets/tn_gm-flag.jpg" alt=""></p>
-            <p v-else>Original Language: {{original_language}}</p>
-            <p>Rate: </p>
-            <img :src="'https://image.tmdb.org/t/p/w185' + item.poster_path" alt="">>
+        <div class="col" v-for="(item, index) in store.seriesArray" :key="index">
+            <AppCard :name="item.name" :original-name="item.original_name" :movieItem="item" />       
         </div>
     </div>
 </template>
