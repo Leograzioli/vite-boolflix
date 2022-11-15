4045365a223cd77fa1a777ee0e314839632ce5fd
Leograzioli <leograzioli@gmail.com>
2022-11-15 10:22:07 +0100

    aggiunto main tag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,26 +18,31 @@ export default {
 </script>
 
 <template>
-    <div v-if="store.moviesArray.length !== 0 || store.seriesArray.length !== 0   " class="container-fluid bg-dark p-5">
-        <h2 class="text-white p-2">Movies:</h2>
-        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 text-white g-3 mb-5">
+    <main>
+        <div v-if="store.moviesArray.length !== 0 || store.seriesArray.length !== 0   " class="container-fluid bg-dark p-5">
+            
             <!-- movie section  -->
-            <div class="col" v-for="(item, index) in store.moviesArray" :key="index">
-                <AppCard :name="item.title" :original-name="item.original_title" :movieItem="item" />       
+            <h2 class="text-white p-2">Movies:</h2>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 text-white g-3 mb-5">
+                <div class="col" v-for="(item, index) in store.moviesArray" :key="index">
+                    <AppCard :name="item.title" :original-name="item.original_title" :movieItem="item" />       
+                </div>
             </div>
-        </div>
-
-        <h2 class="text-white p-2">Series:</h2>
-        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 text-white g-3">
+    
             <!-- serie section  -->
-            <div class="col" v-for="(item, index) in store.seriesArray" :key="index">
-                <AppCard :name="item.name" :original-name="item.original_name" :movieItem="item" />       
+            <h2 class="text-white p-2">Series:</h2>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 text-white g-3">
+            
+                <div class="col" v-for="(item, index) in store.seriesArray" :key="index">
+                    <AppCard :name="item.name" :original-name="item.original_name" :movieItem="item" />       
+                </div>
             </div>
         </div>
-    </div>
-    <div v-else>
-        <h2 class="no-movies">Search a Title</h2>
-    </div>
+        
+        <div v-else>
+            <h2 class="no-movies">Search a Title</h2>
+        </div>
+    </main>
 </template>
 
 <style lang="scss" scoped>
